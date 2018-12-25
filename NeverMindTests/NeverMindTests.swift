14ba16093f25fba4d4eaa62f5e96acfff5206667
Sarah Yan <syan@wellesley.edu>
2018-12-25 10:28:56 -0800

    Initial Commit

---
+++ NeverMindTests/NeverMindTests.swift
@@ -0,0 +1,34 @@
+//
+//  NeverMindTests.swift
+//  NeverMindTests
+//
+//  Created by Sarah Yan on 12/25/18.
+//  Copyright © 2018 MIT Media Labs: Fluid Interfaces. All rights reserved.
+//
+
+import XCTest
+@testable import NeverMind
+
+class NeverMindTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
